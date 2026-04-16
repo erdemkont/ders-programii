0444065749a3c8a53f4d0edb3621a545eeaf9c9e
erdemkont <68051441+erdemkont@users.noreply.github.com>
2026-04-16 17:01:09 +0300

    Upgrade service worker to v3 and add cache management
    Updated service worker to version 3 and added activation logic to delete old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,34 @@
 self.addEventListener("install", e => {
+    self.skipWaiting();
     e.waitUntil(
-        caches.open("ders-programii-v2").then(cache => {
+        caches.open("ders-programii-v3").then(cache => {
             return cache.addAll([
-                "/ders-programii/index.html",
-                "/ders-programii/manifest.json",
-                "/ders-programii/icon.png"
+                "./",
+                "./index.html",
+                "./manifest.json",
+                "./icon.png"
             ]);
         })
     );
 });
 
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== "ders-programii-v3")
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.clients.claim();
+
 self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
         })
     );
 });
